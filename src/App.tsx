e77ea4bafd2c7adec287044f860207e5b2d71eb4
Radim Blaha <radim.blaha@metait.cz>
2021-02-25 23:09:25 +0100

    root route fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FC, Suspense } from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import history from "./utils/history";
 import Users from "./components/Users/Users";
 import UserDetail from "./components/UserDetail/UserDetail";
@@ -10,6 +10,9 @@ const App: FC = () => (
   <Suspense fallback="loading">
     <Router history={history}>
       <Switch>
+        <Route exact path="/">
+          <Redirect to="/users" />
+        </Route>
         <Route exact path={"/users"} component={Users} />
         <Route exact path={"/users/:id"} component={UserDetail} />
         <Route exact path={"/posts"} component={Posts} />
